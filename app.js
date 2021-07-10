5ec80e8cc8e8e33c982344f896c378fae02d1ca5
Dennise Chávez <80653433+dennisechavez@users.noreply.github.com>
2021-07-10 15:52:19 -0400

    Fix port variable.

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = process.env.port || 3000
+const port = process.env.PORT || 3000
 
 // Configure express server
 app.set('view engine', 'pug');
